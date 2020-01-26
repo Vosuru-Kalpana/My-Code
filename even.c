c11ebe49e29d642ed3b9f0d4278a4ccda35c7447
Vosuru-Kalpana <kalpanavosuru@gmail.com>
2020-01-26 11:37:11 +0530

    Add files via upload

---
+++ even.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int
+main()
+{
+    int even(int n)
+    {
+        if(n%2==0)
+        {
+            return 1;
+        }
+        else
+        {  
+            return 0; 
+        }
+    }
+}
